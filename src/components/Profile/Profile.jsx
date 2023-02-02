e19170284c53ff471187ab2df841826094900610
snackli17 <snackli17@gmail.com>
2023-02-02 19:57:17 +0200

    fix task1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,33 @@
-import css from '../Profile/Profile.css'
+import css from "./Profile.module.css";
 import user from '../Profile/user.json';
 
 
 export const Profile = ({username , tag, location, avatar, stats }) => {
-   return <div className="profile">
+  return ( <div className={css.profile}>
   <div className={css.description}>
     <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      src="http://hypeava.ru/uploads/posts/2018-05/1527186603_7.png"
       alt="User avatar"
-      className="avatar"
     />
-           <p className="name">{ user.username}</p>
-           <p className="tag">{ user.tag}</p>
-           <p className="location">{ user.location}</p>
+      <p className={css.name}>{ user.username}</p>
+      <p className={css.tag}>{ user.tag}</p>
+           <p className={css.location}>{ user.location}</p>
   </div>
 
-  <ul className="stats">
-    <li>
-      <span className="label">Followers</span>
-      <span className="quantity">{user.stats.followers}</span>
+     <ul className={css.stats}>
+    <li  className={css.statsList}>
+        <span className={css.label}>Followers</span>
+      <span className={css.quantity}>{user.stats.followers}</span>
     </li>
-    <li>
-      <span className="label">Views</span>
-      <span className="quantity">{user.stats.views}</span>
+    <li  className= {css.statsList}>
+      <span className={css.label}>Views</span>
+         <span className={css.quantity}>{user.stats.views}</span>
     </li>
-    <li>
-      <span className="label">Likes</span>
-      <span className="quantity">{user.stats.likes}</span>
+    <li  className= {css.statsList}>
+         <span className={css.label}>Likes</span>
+         <span className={css.quantity}>{user.stats.likes}</span>
     </li>
   </ul>
-</div>
+  </div>
+  );
 }
