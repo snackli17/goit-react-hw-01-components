95f3f838bcba1a13697c70b17ee5ccb9253fbcf5
snackli17 <snackli17@gmail.com>
2023-02-03 13:34:15 +0200

    text
    text

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,42 @@
-// import PropTypes from 'prop-types';
-import data from '../Statistics/data.json';
+import PropTypes from 'prop-types';
+import {Section,Title, StatList, Item,Label, Percentage  } from './Statistics.styled';
+
+
+let color = '#' + Math.floor(Math.random() * (0xffffff + 1))
+    .toString(16)
+    .padStart(6, '0')
+
+
+
 
 
 export const Statistics = ({title, stats  }) => {
   return (
-   <section class="statistics">
-      <h2 class="title">{title}</h2>
-      <ul>
+   <Section>
+      <Title>{title}</Title>
+      <StatList>
     {stats.map(stat => (
-      <li key={stat.id}>
-      <span class="label">{stat.label}</span>
-      <span class="percentage">{stat.percentage}</span>
-
-      </li>
+      <Item key={stat.id}
+        style={{ background: color }}
+      >
+     
+  
+      <Label>{stat.label}</Label>
+      <Percentage>{stat.percentage}</Percentage>
+      </Item>
     ))}
-  </ul>
-</section>
+  </StatList>
+</Section>
   );
 }; 
+
+Statistics.propTypes = {
+  title:PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage:PropTypes.number.isRequired,
+    }),
+  ),
+};
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,65 @@
+import styled from '@emotion/styled'
+
+
+
+export const Section = styled.section`
+  margin-top: 50px;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color : #BDB76B
+}
+`;
+
+
+export const Title = styled.h1`
+
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+`;
+
+
+
+
+export const StatList = styled.ul`
+  display: flex;
+  flex-direction: row;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+`;
+
+
+
+
+export const Item = styled.li`
+   padding-top: 10px;
+    padding-bottom: 10px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 2px solid #FF69B4;
+}
+`;
+
+
+
+
+export const Label = styled.span`
+   font-size: 20px;
+    color: white;
+    margin-bottom: 5px;
+}
+`;
+
+export const Percentage = styled.span`
+    font-size: 25px;
+    color: white;
+}
+`;
+
